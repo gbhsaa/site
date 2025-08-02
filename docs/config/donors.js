f3641cf8071240860194877c40dc7a1e30340b1b
Md. Rakibul Islam <rakib1708@gmail.com>
2025-08-02 15:09:21 +0600

    added: donor page for alumni

---
+++ docs/config/donors.js
@@ -0,0 +1,69 @@
+export const donorList = 
+[
+    {
+        "name": "ডাঃ মোঃ অলিউল ইসলাম বিপ্লব",
+        "batch": "২০০৩",
+        "image": "./images/committee/biplob.png",
+        "description": "সার্জন, গোবিন্দগঞ্জ উপজেলা স্বাস্থ্য কমপ্লেক্স, গাইবান্ধা",
+    },
+    {
+        "name": "ইঞ্জিঃ শাহ আলম",
+        "batch": "১৯৯৭",
+        "image": "./images/committee/shaalam.png",
+        "description": "ম্যানেজার (অপারেশন) — সিলেট গ্যাস ফিল্ড লিমিটেড"
+    },
+    {
+        "name": "ডাঃ মোঃ শফিউল আলম শামীম",
+        "batch": "১৯৮৯",
+        "image": "./images/committee/shamim.png",
+        "description": "এসটিওপি পরামর্শক, বিশ্ব স্বাস্থ্য সংস্থা (WHO) — আঞ্চলিক কার্যালয়, জাতিসংঘ সিটি, কোপেনহেগেন, ডেনমার্ক"
+    },
+    {
+        "name": "অ্যাড. মশিয়ার রহমান",
+        "batch": "",
+        "image": "./images/committee/mosiar.png",
+        "description": "অ্যাডভোকেট, জর্জকোর্ট, রংপুর"
+    },
+    {
+        "name": "ড. মোঃ কবিরুজ্জামান",
+        "batch": "২০০৩",
+        "image": "./images/committee/kabir.png",
+        "description": "সহকারী অধ্যাপক, আমেরিকান ইন্টারন্যাশনাল ইউনিভার্সিটি-বাংলাদেশ (AIUB)"
+    },
+    {
+        "name": "রবিউল করিম রবিন",
+        "batch": "২০০৩",
+        "image": "./images/committee/rabiul-karim.png",
+        "description": "উপজেলা মৎস্য কর্মকর্তা — মৎস্য ভবন, রমনা, ঢাকা"
+    },
+    {
+        "name": "নাজমুল হুদা",
+        "batch": "১৯৯৬",
+        "image": "./images/committee/nazmul-huda.png",
+        "description": "ডিরেক্টর — ভিশন ইনডাকশন ওয়াডস লিমিটেড"
+    },
+    {
+        "name": "মোঃ শাখাওয়াত হোসেন",
+        "batch": "১৯৮৭",
+        "image": "./images/committee/shakhawat.png",
+        "description": "প্রধান শিক্ষক, গুলাল বুদাই উচ্চ বিদ্যালয়, রংপুর"
+    },
+    {
+        "name": "ইঞ্জিঃ মোঃ রাকিবুল ইসলাম",
+        "batch": "২০১১",
+        "image": "./images/committee/rakib.png",
+        "description": "সফটওয়ার ইঞ্জিনিয়ার, পোরটনিক্স লিমিটেড, ঢাকা"
+    },
+    {
+        "name": "ইঞ্জিঃ মোঃ মোস্তাফিজার রহমান",
+        "batch": "২০০৩",
+        "image": "./images/committee/mostafiz.png",
+        "description": "এক্সিকিউটিভ — স্কয়ার ফ্যাশন ইয়ার্নস লিমিটেড, ঢাকা"
+    },
+    {
+        "name": "ইঞ্জিঃ মোঃ মকসুদার রহমান",
+        "batch": "১৯৯৭",
+        "image": "./images/committee/moksuder.png",
+        "description": "সহকারী প্রকৌশলী — হেক্সিং ইলেকট্রিক্যাল কোম্পানি লিমিটেড"
+    },
+];
